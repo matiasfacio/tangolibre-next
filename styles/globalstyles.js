8b60409916837fb0d59bb6525a45b78c7acb950d
Matias Facio <matiaspersonal@gmail.com>
2021-04-26 19:50:08 +0200

    fixed userarea and useradmin layout

--- styles/globalstyles.js
+++ styles/globalstyles.js
@@ -181,8 +181,8 @@ export const Form = styled.form`
 `;
 
 export const FormContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
